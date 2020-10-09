1dc835e94d8730c63244a500a7ca465f5ce52e0e
Wojtek <wojciech.zbrozek@gmail.com>
2020-10-09 12:57:22 +0200

    Added functionality but still has errors like multiple decimal points

--- app.js
+++ app.js
@@ -1,26 +1,128 @@
+//Variables
+
 let screen = document.getElementById("screen");
-
-screen.innerHTML = "0";
-
-//HTML elements saved to variables
 
 let numberButtons = document.getElementsByClassName("digit");
 let operatorButtons = document.getElementsByClassName("operator");
+let equalsButton = document.getElementById("equals");
+let clearButton = document.getElementById('clear');
+let decimalButton = document.getElementById('decimal');
+let negativeButton = document.getElementById('negative');
+let percentageButton = document.getElementById('percentage');
+
+let pendingValue;
+let currentOperator;
+let currentValue;
+
+screen.innerText = "0";
 
 //Callback functions
 
-let updateVal = (button) => {
-  screen.innerHTML = button.target.innerText;
+let updateScreen = (button) => {
+
+  if (screen.innerText == "-0") {
+    screen.innerText = "-";
+  }
+
+  if (!pendingValue && !currentValue && !currentOperator) {
+    screen.innerText = "";
+    if (button.target.innerText == ".") {
+      screen.innerText = "0";
+    }
+  }
+  if (pendingValue && !currentValue && currentOperator) {
+    screen.innerText = "";
+    if (button.target.innerText == ".") {
+      screen.innerText = "0";
+    }
+  }
+  screen.innerText += button.target.innerText;
+  currentValue = screen.innerText;
+}
+
+
+let setOperator = (button) => {
+  if (!currentOperator) {
+    currentOperator = button.target.innerText;
+    pendingValue = currentValue;
+    currentValue = undefined;
+  }
+  else {
+    currentOperator = button.target.innerText;
+  }
+  if (currentOperator == "÷") { //Compensate for the iPhone notation, which isn't valid operator sign in js mathematical notation
+    currentOperator = "/";
+  }
+  if (currentOperator == "x") { //Compensate for the iPhone notation, which isn't valid operator sign in js mathematical notation
+    currentOperator = "*";
+  }
 }
 
 let clear = () => {
-  screen.innerHTML = "0";
+  screen.innerText = "0";
+  currentValue = undefined;
+  pendingValue = undefined;
+  currentOperator = undefined;
 }
+
+let equals = () => {
+  if (pendingValue && currentValue) {
+    let equation = `${pendingValue}${currentOperator}${currentValue}`;
+    screen.innerText = eval(equation);
+    pendingValue = eval(equation);
+    currentValue = undefined;
+  }
+}
+
+let setNegative = () => {
+  if (!currentValue) {
+    if (pendingValue) {
+      if (screen.innerText.charAt(0) == "-") {
+        screen.innerText = screen.innerText.substring(1);
+        pendingValue = screen.innerText;
+      }
+      else {
+        screen.innerText = "-" + pendingValue;
+        pendingValue = screen.innerText;  
+      }
+    }
+    else {
+    screen.innerText = "-0";
+    currentValue = screen.innerText;  
+    }
+  }
+  else if (screen.innerText.length > 0 && screen.innerText.charAt(0) == "-") {
+    screen.innerText = screen.innerText.substring(1);
+    currentValue = screen.innerText;
+  }
+  else {
+    screen.innerText = "-" + screen.innerText;
+    currentValue = screen.innerText;
+  }
+}
+
+let setPercentage = () => {
+  screen.innerText = eval(screen.innerText/100);
+  currentValue = screen.innerText;
+};
 
 //Event listeners
 
 for (i=0; i<numberButtons.length; i++){
-  numberButtons[i].addEventListener('click', updateVal);
+  numberButtons[i].addEventListener('click', updateScreen);
 };
 
-document.getElementById('clear').addEventListener('click', clear);
+for (i=0; i<operatorButtons.length; i++){
+  operatorButtons[i].addEventListener('click', setOperator);
+};
+
+clearButton.addEventListener('click', clear);
+
+equalsButton.addEventListener('click', equals);
+
+decimalButton.addEventListener('click', updateScreen);
+
+negativeButton.addEventListener('click', setNegative);
+
+percentageButton.addEventListener('click', setPercentage);
+
