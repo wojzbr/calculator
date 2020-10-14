f50a9f4a859d357d114c5eb534a225fab578730c
Wojtek <wojciech.zbrozek@gmail.com>
2020-10-14 21:48:38 +0200

    DRYer approach to logic, orange operators and decimal button seem to work perfectly

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ let decimalButton = document.getElementById('decimal');
 let negativeButton = document.getElementById('negative');
 let percentageButton = document.getElementById('percentage');
 
-let pendingValue;
-let currentOperator;
-let currentValue;
+let pendingValue = "";
+let currentOperator = "";
+let currentValue = "";
 let isDecimalSet = false;
 
 screen.innerText = "0";
@@ -20,119 +20,64 @@ screen.innerText = "0";
 //Callback functions
 
 let updateScreen = (button) => {
-
-  if (screen.innerText == "-0") {
-    screen.innerText = "-";
+  if (screen.innerText === "0" && button.target.innerText === "0") { // blocks from writing multiple 0s in front of the number like 0000.2 (redundant)
+    return;
   }
-  else if (screen.innerText == "0" && !isDecimalSet) {
-    screen.innerText = "";
+  if (currentOperator && !currentValue) {  //zeros out the text on the screen to start building another number
+    screen.innerText = "";  
   }
-
-  else if (!pendingValue && !currentValue && !currentOperator) {
-    screen.innerText = "";
-  }
-  else if (pendingValue && !currentValue && currentOperator) {
-    screen.innerText = "";
+  if (screen.innerText.charAt(0) === "0" && !screen.innerText.includes('.')) { //If the number is NOT a decimal and it has 0 in front, the zero is removed
+    screen.innerText = screen.innerText.slice(1);
   }
   screen.innerText += button.target.innerText;
   currentValue = screen.innerText;
 }
 
-
 let setOperator = (button) => {
-  if (!currentOperator) {
-    currentOperator = button.target.innerText;
-    pendingValue = currentValue;
-    currentValue = undefined;
-    isDecimalSet = false;
-  }
-  else if (pendingValue && currentValue && currentOperator) {
-    equals();
-  }
-  else {
-    currentOperator = button.target.innerText;
-  }
-  if (currentOperator == "÷") { //Compensate for the iPhone notation, which isn't valid operator sign in js mathematical notation
-    currentOperator = "/";
-  }
-  if (currentOperator == "x") { //Compensate for the iPhone notation, which isn't valid operator sign in js mathematical notation
-    currentOperator = "*";
-  }
+  currentOperator = button.target.innerText;
+  pendingValue = screen.innerText;
+  currentValue = "";
+  console.log("operator set")
 }
 
 let clear = () => {
+  pendingValue = "";
+  currentValue = "";
+  currentOperator = "";
   screen.innerText = "0";
-  currentValue = undefined;
-  pendingValue = undefined;
-  currentOperator = undefined;
 }
 
 let equals = () => {
-  if ((pendingValue || pendingValue=="0") && currentValue) {
-    let equation = `${pendingValue}${currentOperator} ${currentValue}`;
-    screen.innerText = eval(equation);
-    pendingValue = eval(equation);
-    currentValue = undefined;
-    isDecimalSet = false;
+  if (currentOperator === "+") {
+    screen.innerText = (parseFloat(pendingValue)+parseFloat(currentValue)).toString();
+  }
+  if (currentOperator === "-") {
+    screen.innerText = (parseFloat(pendingValue)-parseFloat(currentValue)).toString();
+  }
+  if (currentOperator === "*") {
+    screen.innerText = (parseFloat(pendingValue)*parseFloat(currentValue)).toString();
+  }
+  if (currentOperator === "/") {
+    screen.innerText = (parseFloat(pendingValue)/parseFloat(currentValue)).toString();
   }
 }
 
 let setDecimal = () => {
-  if (!isDecimalSet) {
-    if (currentValue) {
-      screen.innerText += ".";
-      currentValue = screen.innerText;
-      isDecimalSet = true;
-    }
-    if (!currentValue) {
-      screen.innerText = "0.";
-      currentValue = screen.innerText;
-      isDecimalSet = true;
-    }
+  if (screen.innerText.includes('.')) {
+    return;
+  }
+  else {
+    screen.innerText += ".";
   }
 }
 
 let setNegative = () => {
-  if (currentOperator && !currentValue) {
-    screen.innerText = "-0";
-    currentValue = screen.innerText;  
-  }
-  else if (!currentValue) {
-    if (pendingValue) {
-      if (screen.innerText.charAt(0) == "-") {
-        screen.innerText = screen.innerText.substring(1);
-        currentValue = screen.innerText;
-      }
-      else if (currentValue) {
-        screen.innerText = "-" + currentValue;
-        currentValue = screen.innerText;  
-      }
-    }
-    else {
-    screen.innerText = "-0";
-    currentValue = screen.innerText;  
-    }
-  }
-  else if (screen.innerText.length > 0 && screen.innerText.charAt(0) == "-") {
-    screen.innerText = screen.innerText.substring(1);
-    currentValue = screen.innerText;
-  }
-  else {
-    screen.innerText = "-" + screen.innerText;
-    currentValue = screen.innerText;
-  }
+
 }
 
 let setPercentage = () => {
-  if (currentValue) {
-    screen.innerText = eval(currentValue/100);
-    currentValue = screen.innerText;
-  }
-  else if (pendingValue) {
-    screen.innerText = eval(pendingValue/100);
-    pendingValue = screen.innerText;
-  }
-};
+
+}
 
 //Event listeners
 
